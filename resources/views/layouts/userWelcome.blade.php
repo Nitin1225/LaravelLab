24e18e286ef3b138ad9e20e9c3296f15cdeaac3d
Nitin Kumar <connect.nitinkumar@gmail.com>
2020-02-19 22:04:31 +0530

    New user welcome

--- resources/views/layouts/userWelcome.blade.php
+++ resources/views/layouts/userWelcome.blade.php
@@ -2,15 +2,47 @@
 
     @section('containerContent')
 
-        <h2> Detials </h2>
+        <!-- <h2> User Detials </h2> -->
 
-        @foreach($postData as $key=>$value)
-        
+        <div class="">
+                <div class="modal-header">
+                    <button type="button" class="close" ></button>
+                    <h2 class="modal-title" id="myModalLabel" align="center">User Detials</h2>
+                </div>
+                <div class="modal-body">
+                    <center>
+                        <img src="{{asset($postData['image'])}}" name="Profile Pic" width="140" height="140" border="0" class="img-circle"></a>
+                        <h3 class="media-heading">{{$postData['name']}}</h3>
+                        <!-- <span><strong>Skills: </strong></span>
+                            <span class="label label-warning">HTML5/CSS</span>
+                            <span class="label label-info">Adobe CS 5.5</span>
+                            <span class="label label-info">Microsoft Office</span>
+                            <span class="label label-success">Windows XP, Vista, 7</span> -->
+                    </center>
+                    <hr>
+                    <center>
+                        <p class="text-center"><strong>Information : </strong><br>
+                            <span class="label label-info">Email : {{$postData['email']}}</span>
+                            <span class="label label-warning">Password : {{$postData['pass1']}}</span>
+                            <!-- <span class="label label-warning">{{$postData['pass']}}</span> -->
+                            <span class="label label-info">Contact No. : {{$postData['mobile']}}</span>
+                        </p>
+                    <br>
+                    </center>
+                </div>
+                <div class="footer">
+                    <center>
+                        
+                    </center>
+                </div>
+        </div>
+
+        <!-- @foreach($postData as $key=>$value)
             <div class="container">
                 @if($key=="image")
                     <h4>
                         {{$key}} : 
-                            <img src = "{{asset($value)}}" altname="Profile Image" height="150px" width="150px">
+                            <img src = "{{asset($value)}}" alt="Profile Image" height="150px" width="150px">
                             <br>
                             &emsp; {{$value}}
                     </h4>
@@ -18,7 +50,6 @@
                     <h4>{{$key}} : {{$value}}</h4>
                 @endif
             </div>
-
-        @endforeach
+        @endforeach -->
 
     @endsection
